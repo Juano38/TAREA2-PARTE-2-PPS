48fbdae6ae782b30bb714e58bcf28ac8362fb389
Juano38 <juangarcia.25@campuscamara.es>
2025-10-24 17:18:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var WebSocketServer = require('ws').Server
-var http = require('http');
+var http = require('https');
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
